e5f146392ee1f09779981d609805a66b2f923857
KarimElAfifi <99023670+KarimElAfifi@users.noreply.github.com>
2026-01-12 01:33:37 +0100

    Day 2

---
+++ 2.py
@@ -0,0 +1,29 @@
+import re
+
+with open("IDs.txt", "r") as file:
+    id = file.readline()
+    strID = id.split(',')
+
+# finding invalid IDs 
+# safing them in invalid_ids list
+# adding their values to sum
+def find_invalid_ID(strID):
+    invalid_ids = []
+
+    for i in strID:
+        i = i.strip()
+
+        numRange = re.search(r'(\d+)-(\d+)', i)
+        
+        minNum = int(numRange.group(1))
+        maxNum = int(numRange.group(2))
+
+        for x in range(minNum, maxNum + 1):
+            if re.fullmatch(r'(.+)\1', str(x)):
+                invalid_ids.append(x)
+
+    total = sum(invalid_ids)
+    return total
+
+
+print(find_invalid_ID(strID))
