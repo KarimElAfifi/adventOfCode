a0d25700de9b0fe85a8546a2818f796f2c6b4eca
KarimElAfifi <99023670+KarimElAfifi@users.noreply.github.com>
2026-01-16 01:24:11 +0100

    cleanup

--- day3/day3PartTwo.py
+++ day3/day3PartTwo.py
@@ -4,12 +4,13 @@ with open ("day3/batteryInput.txt", "r") as file:
 def find_highest_batteryCombination(battery):
     total = 0
 
+    for line in battery:
+        line = line.strip()
+
+#----------------------------------------
     # this is just a safe guard to ensure we only process 
     # lines with at least 12 digits.
     # But here, it is optional, so its not "necessary"
-#----------------------------------------
-    for line in battery:
-        line = line.strip()
 
         if not line or len(line) < 12:
             continue
@@ -17,8 +18,7 @@ def find_highest_batteryCombination(battery):
         remove_count = len(line) - 12
 #----------------------------------------
 
-        stack = []
-        
+        stack = []        
         for digit in line:
             # Remove smaller digits if the next digit is larger
             while stack and remove_count > 0 and stack[-1] < digit:
@@ -30,8 +30,8 @@ def find_highest_batteryCombination(battery):
         while remove_count > 0:
             stack.pop()
             remove_count -= 1
-        
-        # join fucntion to put the digits together
+
+        # join function to put the digits together
         # then convert to integer
         # finally add to total
         joltage = int(''.join(stack))
