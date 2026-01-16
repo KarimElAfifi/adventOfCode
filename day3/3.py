ed61e4beb1ffa071841ab302c9ebce701ddbadde
KarimElAfifi <99023670+KarimElAfifi@users.noreply.github.com>
2026-01-16 01:04:14 +0100

    day 3 part two

--- day3/3.py
+++ day3/3.py
@@ -14,12 +14,12 @@ def find_highest_batteryCombination(battery):
         max_joltage = 0
         for i in range(len(line) - 1):
             for j in range(i + 1, len(line)):
-                joltage = int(line[i]) * 10 + int(line[j])
+                joltage = int((line[i]) * 10 + int(line[j]))
 
                 max_joltage = max(max_joltage, joltage)
-        
+
         total += max_joltage
-            
+
     return total
 
 print(find_highest_batteryCombination(battery))
---
+++ day3/day3PartTwo.py
@@ -0,0 +1,35 @@
+with open ("day3/batteryInput.txt", "r") as file:
+    battery = file.readlines()
+
+def find_highest_batteryCombination(battery):
+    total = 0
+
+    for line in battery:
+        line = line.strip()
+
+        if not line or len(line) < 12:
+            continue
+        
+        remove_count = len(line) - 12
+        stack = []
+        
+        for digit in line:
+            # Remove smaller digits if the next digit is larger
+            while stack and remove_count > 0 and stack[-1] < digit:
+                stack.pop()
+                remove_count -= 1
+            stack.append(digit)
+        
+        # If we still need to remove digits, remove from the end
+        while remove_count > 0:
+            stack.pop()
+            remove_count -= 1
+        
+        joltage = int(''.join(stack))
+        total += joltage
+
+    return total
+
+print(find_highest_batteryCombination(battery))
+
+
